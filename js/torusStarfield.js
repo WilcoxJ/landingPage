456935b82ecc957cfb4639f80abc983204233ced
Josh Wilcox <wilcoxjosh505@gmail.com>
2022-05-03 14:40:28 -0600

    trail fx

--- js/torusStarfield.js
+++ js/torusStarfield.js
@@ -1,7 +1,3 @@
-// script3
-
-
-// Make highly-transparent plane
 var fadeMaterial = new THREE.MeshBasicMaterial({
     color: 0x000000,
     transparent: true,
@@ -10,39 +6,26 @@ var fadeMaterial = new THREE.MeshBasicMaterial({
 var fadePlane = new THREE.PlaneBufferGeometry(10, 10);
 var fadeMesh = new THREE.Mesh(fadePlane, fadeMaterial);
 
-// Create Object3D to hold camera and transparent plane
 var camGroup = new THREE.Object3D();
-//var camera = new THREE.PerspectiveCamera();
-
 var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
 camGroup.add(camera);
 camGroup.add(fadeMesh);
 
-// Put plane in front of camera
 fadeMesh.position.z = -0.08;
-
-// Make plane render before particles
 fadeMesh.renderOrder = -1;
-
 
 const canvas = document.createElement('canvas');
 const scene = new THREE.Scene();
 // const renderer = new THREE.WebGLRenderer({ canvas, antialias: true, alpha: true });
 renderer = new THREE.WebGLRenderer( { alpha: true, preserveDrawingBuffer: true, antialias: true } );
 
-// renderer.setSize(window.innerWidth, window.innerHeight);            
-
 renderer.setClearColor(0x00000, 1);  
-
 // renderer.autoClearColor = false;
 renderer.setSize(window.innerWidth, window.innerHeight);
 
 document.body.appendChild(renderer.domElement);
 
-
-
-// Add camGroup to scene
 scene.add(camGroup);
 camGroup.position.z = 60;
 
@@ -60,26 +43,17 @@ const particlesMaterial = new THREE.PointsMaterial({
     });
 particlesMaterial.opacity = .90;
 
-
-
 // geometry
 const geometry = new THREE.TorusGeometry( 13, 5, 25, 100 );
 const geometry2 = new THREE.BoxGeometry( 30, 30, 30 );
-
 
 const sphere = new THREE.Points( geometry, material );
 scene.add( sphere );
 const cube = new THREE.Points( geometry2, material );
 
-
 // Light
 const light = new THREE.HemisphereLight(0xf402bc, 0x236e89, .5);
 scene.add(light);
-
-// const frontLight = new THREE.DirectionalLight(0xb6ebef, .75);
-// frontLight.position.set(3000, 500, 3000).normalize();
-// scene.add(frontLight);
-
 
 window.addEventListener( 'resize', onWindowResize, false );
 
@@ -90,22 +64,17 @@ function onWindowResize(){
 
 }
 
-
-
 //particles
 const particleGeometry = new THREE.BufferGeometry;
 const particlesCount = 20000;
 const posArray = new Float32Array(particlesCount * 3);
-
 
 for(let i = 0; i < particlesCount * 3; i++) {
     posArray[i] = (math.random() - 0.5) * 125;
 }
 
 particleGeometry.setAttribute('position', new THREE.BufferAttribute(posArray, 3));
-
 const particlesMesh = new THREE.Points(particleGeometry, particlesMaterial);
-
 scene.add( cube, particlesMesh );
 
 //mouse
@@ -126,7 +95,6 @@ function onDocumentMouseWheel(event) {
 
 }
 
-
 function onDocumentMouseDown(event) {
         renderer.autoClearColor = false; // trails 
         fadeMesh.position.z = -0.12;
@@ -139,14 +107,12 @@ function onDocumentMouseUp(event) {
 
   }
 
-
 function onDocumentClick(event) {
     renderer.autoClearColor = true;
     renderer.setClearColor(0x00000, 1); 
     material.color = 0xff00dc;
 
 }
-
 
 function animateParticles(event) {
   mouseX = event.clientX;
@@ -156,7 +122,6 @@ function animateParticles(event) {
 
 const clock = new THREE.Clock();
 const elapsedTime = clock.getElapsedTime;
-
 
 const animate = function () {
   requestAnimationFrame( animate );
